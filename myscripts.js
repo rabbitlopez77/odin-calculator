1ae081b23293e4cf4357721b9cd996f1aeb90970
rabbitlopez77 <rabbitlopez22@gmail.com>
2022-12-27 17:32:16 -0800

    Finished the adding

---
+++ myscripts.js
@@ -0,0 +1,30 @@
+let userNumber = [];
+let userNumber2 = [];
+let userStart = true;
+const displayNumber=(num)=>{
+    if(userStart){
+        userNumber.push(num)
+        document.getElementById("display1").innerHTML = userNumber.join('');
+
+    }else if(!userStart)
+    {
+        userNumber2.push(num);
+        document.getElementById("display2").innerHTML = userNumber2.join('');
+    }
+}
+let currentExpression = '';
+const chosenExperssion =(exp)=>{
+    currentExpression = exp;
+    userStart = false;
+}
+const sumExpression = (num1, num2) => {
+    return num1 + num2
+}
+const equals =()=>{
+    switch(currentExpression) {
+        case 'add':
+            {var finalSum = sumExpression(Number((userNumber.join(''))), Number((userNumber2.join(''))))};
+            document.getElementById('display3').innerHTML = finalSum;
+            break;
+    }
+}
