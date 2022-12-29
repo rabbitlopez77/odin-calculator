5727a2e7a9c522991f02559e5f8e670811d435d2
rabbitlopez77 <rabbitlopez22@gmail.com>
2022-12-28 20:43:14 -0800

    almost there  updated bbuttons and functions

--- myscripts.js
+++ myscripts.js
@@ -3,22 +3,27 @@ let userNumber2 = '';
 let userStart = true;
 let currentExpression = '';
 const calculator = document.querySelector('#calculator');
+const display1 =document.getElementById('display1');
+const display2 =document.getElementById('display2');
+const display3 =document.getElementById('display3');
 const displayNumber=(num)=>{
     if(userStart){
         userNumber += num
-        document.getElementById('display3').innerHTML = 0;
-        document.getElementById("display1").innerHTML = userNumber;
+        display3.innerHTML = 0;
+        display1.innerHTML = userNumber;
         return userNumber
 
     }else if(!userStart)
     {
         userNumber2 += num;
-        document.getElementById('display3').innerHTML = 0;
-        document.getElementById("display2").innerHTML = userNumber2;
+        display3.innerHTML = 0;
+        display2.innerHTML = userNumber2;
         return userNumber2
     }
 }
 const chosenExperssion =(exp)=>{
+    if(userNumber === '')
+        return;
     currentExpression = exp;
     changeCalColor(currentExpression)
     userStart = false;
@@ -39,9 +44,9 @@ const equals =()=>{
     switch(currentExpression) {
         case 'add':
             var finalSum = sumExpression(Number(userNumber), Number(userNumber2));
-            document.getElementById('display3').innerHTML = finalSum;
-            document.getElementById("display1").innerHTML = 0;
-            document.getElementById("display2").innerHTML = 0;
+            display3.innerHTML = finalSum;
+            display1.innerHTML = 0;
+            display2.innerHTML = 0;
             userNumber = '';
             userNumber2 = '';
             userStart = true;
@@ -50,9 +55,9 @@ const equals =()=>{
             break;
         case 'subtract':
             {var finalSub = subExpression(Number(userNumber), Number(userNumber2))}
-            document.getElementById('display3').innerHTML = finalSub;
-            document.getElementById("display1").innerHTML = 0;
-            document.getElementById("display2").innerHTML = 0;
+            display3.innerHTML = finalSub;
+            display1.innerHTML = 0;
+            display2.innerHTML = 0;
             userNumber = '';
             userNumber2 = '';
             userStart = true;
@@ -61,9 +66,9 @@ const equals =()=>{
             break;
         case 'multiply':
             {var finalMul = mulExpression(Number(userNumber), Number(userNumber2))};
-            document.getElementById('display3').innerHTML = finalMul;
-            document.getElementById("display1").innerHTML = 0;
-            document.getElementById("display2").innerHTML = 0;
+            display3.innerHTML = finalMul;
+            display1.innerHTML = 0;
+            display2.innerHTML = 0;
             userNumber = '';
             userNumber2 = '';
             userStart = true;
@@ -72,9 +77,9 @@ const equals =()=>{
             break;
         case 'divide':
             {var finaldiv = divExpression(Number(userNumber), Number(userNumber2))};
-            document.getElementById('display3').innerHTML = finaldiv;
-            document.getElementById("display1").innerHTML = 0;
-            document.getElementById("display2").innerHTML = 0;
+            display3.innerHTML = finaldiv;
+            display1.innerHTML = 0;
+            display2.innerHTML = 0;
             userNumber = '';
             userNumber2 = '';
             userStart = true;
@@ -101,4 +106,28 @@ const changeCalColor = (exp) => {
             calculator.style.backgroundColor = 'white';
             break;
     }
+}
+const allClear=()=>{
+    userNumber = '';
+    userNumber2 = '';
+    userStart = true;
+    currentExpression = '';
+    display1.innerHTML = 0;
+    display2.innerHTML = 0;
+    display3.innerHTML = 0;
+    changeCalColor(0)
+}
+const backspace =()=>{
+    if(userStart){
+        userNumber = userNumber.slice(0, -1);
+        if(userNumber === '')
+            return display1.innerHTML = 0;
+        display1.innerHTML = userNumber;
+    }else if(!userStart){
+        userNumber2 = userNumber2.slice(0, -1)
+        if(userNumber2 === '')
+            return display2.innerHTML = 0;
+        display2.innerHTML = userNumber2;
+    }
+        
 }
